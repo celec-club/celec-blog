46656621e2b0f4ea362a1855d4501a1203ed768b
LAGGOUNE-Walid <walidlaggoune159@gmail.com>
2022-10-07 23:25:50 +0100

    add more pixels to image resizer

--- src/app/Actions/Image/CreateMultipleResizedImagesForBlogAction.php
+++ src/app/Actions/Image/CreateMultipleResizedImagesForBlogAction.php
@@ -9,9 +9,9 @@ class CreateMultipleResizedImagesForBlogAction
 {
     public function handle(Blog $blog, string $fullCoverPath, string $slug): void
     {
-        $smallCoverPath = 'covers/'.$slug.'-324x200-cover.jpeg';
+        $smallCoverPath = 'covers/'.$slug.'-360x250-cover.jpeg';
         Image::load(storage_path().'/app/public/'.$fullCoverPath)->width(360)->height(250)->save('storage/app/public/'.$smallCoverPath);
-        $mediumCoverPath = 'covers/'.$slug.'-544x350-cover.jpeg';
+        $mediumCoverPath = 'covers/'.$slug.'-600x400-cover.jpeg';
         Image::load(storage_path().'/app/public/'.$fullCoverPath)->width(600)->height(400)->save('storage/app/public/'.$mediumCoverPath);
         $blog->image()->create(['path' => $smallCoverPath, 'size' => 'small']);
         $blog->image()->create(['path' => $mediumCoverPath, 'size' => 'medium']);
