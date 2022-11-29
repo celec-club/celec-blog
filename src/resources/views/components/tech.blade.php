3535789e17f305fa80ca77e849dce100e04f15b2
LAGGOUNE Walid <walidlaggoune159@gmail.com>
2022-11-29 18:11:24 +0100

    added parallex scroll

--- src/resources/views/components/tech.blade.php
+++ src/resources/views/components/tech.blade.php
@@ -1,6 +1,12 @@
 <div class="container">
     {{-- <div class="row" style="border-radius: 8px; background-color: rgb(11, 8, 53);"> --}}
-    <div class="row" style="border-radius: 18px; background-image: url('{{ url('public/137980.jpg') }}'); background-size: cover; background-position: center; border-bottom: 3px solid #8c8c8c">
+    <div class="row" style="
+        border-radius: 18px; 
+        background-image: url('{{ url('public/137980.jpg') }}'); 
+        background-size: cover; 
+        background-position: center; 
+        background-attachment: fixed;
+        border-bottom: 3px solid #8c8c8c">
         <div class="col p-5 text-white text-center">
             <ul class="list-inline">
                 <li class="list-inline-item pe-5 pt-3"><a href="https://httpd.apache.org/"><img src="{{ url('public/apache-server.png') }}" alt="" width="200px"></a></li>
